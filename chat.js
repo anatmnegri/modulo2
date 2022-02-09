72f070ce21c2ab0bc0598f6a10460f7da06589fe
anatmnegri <anateresamn@gmail.com>
2022-02-08 21:35:54 -0300

    atividade if else

--- chat.js
+++ chat.js
@@ -1,13 +1,47 @@
+// const input = require("readline-sync");
+// // esta entrada permite que a variável input utilize a dependência readline-sync.
+// //O comando require é usado antes da dependência.
+
+// console.log("Seja bem vinde! Vamos começar seu cadastro")
+
+// const idade = input.question("Qual sua idade? ");
+
+// if (idade >=18 && idade <21) {
+//     console.log("Pode entrar")
+// } else if (idade >=21) {
+//     console.log("Pode entrar e consumir bebidas alcolicas")
+// } else {
+//     console.log("Entrada permitida para maiores de 18 anos")
+// }
+
+// const input = require("readline-sync");
+
+// console.log("Seja bem vinde! Vamos verificar sua obrigatoriedade de voto")
+
+// const idade = input.question("Qual sua idade? ");
+
+// if (idade >=16 && idade <18) {
+//     console.log("Já pode votar, porém não é obrigatório.")
+// } else if (idade >=18) {
+//     console.log("Voto obrigatório. Tire seu Título de Eleitor")
+// } else {
+//     console.log("Ainda não está na idade de votar.")
+// }
+
+
 const input = require("readline-sync");
+console.log("Seja bem vinde! Vamos verificar sua situação academica");
 
-console.log("Seja bem vinde! Vamos começar seu cadastro")
+const nota1 = parseInt(input.question("Qual 1 nota? "));
+const nota2 = parseInt(input.question("Qual 2 nota? "));
+const nota3 = parseInt(input.question("Qual 3 nota? "));
 
-const idade = input.question("Qual sua idade? ");
+const media = (nota1 + nota2 + nota3)/3
 
-if (idade >=18 && idade <21) {
-    console.log('Pode entrar')
-} else if (idade >=21) {
-    console.log('Pode entrar e consumir bebidas alcolicas')
+if (media >= 7) {
+    console.log("Aprovado.")
+} else if (media >= 3 && media < 7) {
+    console.log("Recuperação.")
 } else {
-    console.log('Entrada permitida para maiores de 18 anos')
+    console.log("Reprovado.")
 }
