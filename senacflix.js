463583b8139a153ba7bf613e0b2df6b5fd5a0c15
anatmnegri <anateresamn@gmail.com>
2022-02-17 20:20:02 -0300

    ajuste de arquivos

--- senacflix.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-    console.lo('Segundo')
-    console.log('Primeiro');
-    }, 3000);
- 
-    // setTimeout(function() {
-    //     console.log('Primeiro');
-    //     }, 3000);
-    //  console.log('Segundo');
